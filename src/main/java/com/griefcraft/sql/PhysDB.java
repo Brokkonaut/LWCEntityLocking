cefe865787351d603677c3e9cc9e649a1b05a9c0
Brokkonaut <hannos17@gmx.de>
2021-12-11 04:43:49 +0100

    Fix for 1.18

--- src/main/java/com/griefcraft/sql/PhysDB.java
+++ src/main/java/com/griefcraft/sql/PhysDB.java
@@ -836,7 +836,7 @@ public class PhysDB extends Database {
             protection.setX(x);
             protection.setY(y);
             protection.setZ(z);
-            if (blockId == EntityBlock.ENTITY_BLOCK_ID || y < 0 || y > 255) { // this is a bit ugly but fixes broken protections
+            if (blockId == EntityBlock.ENTITY_BLOCK_ID || y < -64 || y > 319) { // this is a bit ugly but fixes broken protections
                 protection.setIsEntity(true);
             } else {
                 protection.setBlockMaterial(BlockMap.instance().getMaterial(blockId));
